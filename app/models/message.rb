be0d780785049e1b1adfcf4f5c4c76d4fdab6d8e
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-14 10:55:54 +0100

    Add JS to submit message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true
+
+  scope :recent, -> { order(:created_at).last(10) }
+  # Ex:- scope :active, -> {where(:active => true)}
 end
